1bba8445d9198d56ee92fb9c97bebf2cc06156a0
Mr-Macko <max.allesch@gmx.de>
2021-11-28 21:08:08 +0100

    added example movie data set

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
 // requires express module
 const express = require ('express');
 const app = express();
+
+let topMovies = [
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+    {
+        title: 'Movie',
+        genre: 'Genre',
+        actor: 'Actor',
+    },
+];
