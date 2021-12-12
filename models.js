518e6ebbcadf0d42c806ef950cb6fec2d9fba752
Mr-Macko <max.allesch@gmx.de>
2021-12-12 16:02:39 +0100

    added movie schema

--- models.js
+++ models.js
@@ -1,3 +1,18 @@
 // requires mongoose
 const mongoose = require ('mongoose')
 
+// Movie Schema
+let movieSchema = mongoose.Schema({
+    Title: {type: Stringm, required: true},
+    Description: {type: String, required: ture},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
