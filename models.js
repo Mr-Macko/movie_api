eefe2f57b6f171c7f7c7a51ff3184b40793038d2
Mr-Macko <max.allesch@gmx.de>
2021-12-22 19:26:52 +0100

    Update models.js

--- models.js
+++ models.js
@@ -26,7 +26,7 @@ let movieSchema = mongoose.Schema({
 let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
-    Email: {type: String, required: true},
+    email: {type: String, required: true},
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
 });
