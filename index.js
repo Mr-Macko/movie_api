7909fec2fb87199f040fdcefa0bd0d2f6fbe2453
Mr-Macko <max.allesch@gmx.de>
2022-02-03 00:59:21 +0100

    updated allowedOrigins

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.static('public'));
 
 // requires CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234','*'];
+let allowedOrigins = ['http://localhost:1234','*','https://max-myflix.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
